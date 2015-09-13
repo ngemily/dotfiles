a38786c2538a59c5322dbe7969df8be30a19d6fb
Emily Ng <ngemily2@gmail.com>
2015-09-13 13:02:06 -0400

    add script to do symlinking; may become more thorough install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+CWD=`pwd`
+ln -fs ${CWD}/vimrc ~/.vimrc
+ln -fs ${CWD}/vim ~/.vim
+ln -fs ${CWD}/zshrc ~/.zshrc
+ln -fs ${CWD}/aliases ~/.aliases
+ln -fs ${CWD}/tmux.conf ~/.tmux.conf
+ln -fs ${CWD}/powerline ~/.config/powerline
+ln -fs ${CWD}/ssh/config ~/.ssh/config
